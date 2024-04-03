6391ba12631f96a09729ccaf8919acc0dbcef541
ayaalakwaa <153412822+ayaalakwaa@users.noreply.github.com>
2024-04-03 10:55:09 +0000

    Family Static

--- src/app.py
+++ src/app.py
@@ -25,18 +25,69 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    if members:
+        response_body = {
+            "family": members
+        }
+        return jsonify(response_body), 200
+    else:
+        return jsonify({"message": "Members not found"}), 404
+    
 
 
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def handle_add():
+    new_member = request.json
+    if new_member:
+        added_member = jackson_family.add_member(new_member)
+        response_body = {
+            "member": added_member
+        }
+        return jsonify(response_body), 200
+    else:
+        return jsonify({"message": "New member not found"}), 404
+
+
+
+@app.route('/member/<int:id>', methods=['GET'])
+def handle_get_specific_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        response_body = {
+            "name": f"{member['first_name']} {member['last_name']}",
+            "id": member['id'],
+            "age": member['age'],
+            "lucky_numbers": member['lucky_numbers']
+        }
+        return jsonify(response_body), 200
+    else:
+        return jsonify({"message": "Member not found"}), 404
+
+    
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def handle_delete_member(id):
+    member = jackson_family.delete_member(id)
+    if member:
+        response_body = {
+            "done": True
+        }
+        return jsonify(response_body), 200
+    else:
+        return jsonify({"message": "Member not found"}), 404
+    
+
+    
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
